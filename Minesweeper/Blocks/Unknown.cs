fd21ccf6476d730c3a2b0ed34fcb284d1fb1b7fa
彥凱 程 <dcvsling@dcvs.idv.tw>
2020-08-05 08:33:04 +0800

    加入專案檔案。

---
+++ Minesweeper/Blocks/Unknown.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Mines
+{
+    public class Unknown : IBlock
+    {
+        private readonly IBlock _block;
+        private readonly IGameControl _control;
+        private static Func<IBlock, IBlock> _active;
+        public Unknown(IBlock block, IGameControl control)
+        {
+            _block = block;
+            _control = control;
+            _active = Initialize;
+        }
+        public bool IsBomb => _block.IsBomb;
+        public Point Location => _block.Location;
+        public DisplayKind Display => DisplayKind.UnKnown;
+
+        public IBlock Active()
+            => _active(_block);
+
+        private IBlock ActiveBlock(IBlock block)
+        {
+            _control.SetBlock(block);
+            _control.SetBomb(block);
+            block = block.Active();
+            if(block.Display == DisplayKind.Clear)
+                _control.GetNearBlocks(block.Location).Each(b => b.Active());
+            return block;
+        }
+
+        private IBlock Initialize(IBlock first)
+        {
+            _control.SetBomb(first);
+            _active = ActiveBlock;
+            return _active(first);
+        }
+
+        public IBlock ToggleFlag()
+            => _control.SetBlock(new Flag(this, _control));
+    }
+}
---
+++ Minesweeper/GameControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+
+namespace Mines
+{
+    [DebuggerDisplay("{DebugView}")]
+    public class GameControl : IGameControl
+    {
+        private readonly IDictionary<Point, IBlock> _gameMap;
+        private Action<IBlock> _setBomb;
+        
+        public GameControl(MinesOptions options)
+        {
+            Size = options.Size;
+            MineCount = options.MineCount;
+            _setBomb = SetFirstBomb;
+            _gameMap = this.CreateMap();
+        }
+        public Size Size { get; }
+        public int MineCount { get; }
+        public int FlagCount => MineCount - FlagLocation.Count;
+        public IList<Point> FlagLocation { get; } = new List<Point>();
+
+        public IBlock GetBlock(Point location) => _gameMap[location];
+        public void SetFlag(IBlock block)
+        {
+            FlagLocation.Add(block.Location);
+            _gameMap[block.Location] = new Flag(block, this);
+        }
+        public void ActiveBlock(IBlock block)
+        {
+            _gameMap[block.Location] = block;
+            block.Active();
+        }
+        public IBlock SetBlock(IBlock block) => _gameMap[block.Location] = block;
+        public void HitBomb(Point location) => _gameMap.Values.Each(b => b.Active());
+        public Action<IBlock> SetBomb => _setBomb;
+
+        private void SetFirstBomb(IBlock first)
+        {
+            _setBomb = _ => { };
+            _gameMap.SetBomb(this, first);
+        }
+
+        public void Reset()
+        {
+            _gameMap.Clear();
+            this.CreateMap().Each(x => _gameMap.Add(x.Key, x.Value));
+        }
+
+        internal string DebugView
+            => string.Join(Environment.NewLine, _gameMap.Values.GroupBy(x => x.Location.Y, x => x.Display.ChangeDisplay)
+                .Select(x => string.Concat(x)));
+    }
+}
